3f8a4abc0b21365553ec90eb2cc2dbad17ec36db
Sergej Pejovic <s.pejovic121@gmail.com>
2025-04-11 14:30:57 +0200

    Dodat header

---
+++ src/app/components/helpers/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class HeaderComponent {}
